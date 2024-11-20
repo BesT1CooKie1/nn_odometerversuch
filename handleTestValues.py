109a6d5d5ab65fc003123a24e8bc0ce171037ab2
Lukas Hellmiß <Lukas.Hellmiss@hcu-hamburg.de>
2024-11-20 12:54:14 +0100

    Test commit

--- handleTestValues.py
+++ handleTestValues.py
@@ -32,7 +32,7 @@ def generate_test_values():
     e0 = generate_initial_porosity()
     sigma0 = generate_initial_stress()
     delta_sigma = generate_additional_stress()
-    # testssd4s
+    # testssd4sss
     return {
         "Cc": Cc,
         "Cs": Cs,
